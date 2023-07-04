7417740ac81e19bd7e6e74ac9e99938bb47b8946
Mohammad Al Nawasrah <nawasrahmohammad2000@gmail.com>
2023-07-04 03:15:34 +0300

    add test api to ctreate new user

---
+++ src/main/java/com/KG/KGMS/test2/UserService.java
@@ -0,0 +1,24 @@
+package com.KG.KGMS.test2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    // Other methods for manipulating user data
+    // ...
+}
